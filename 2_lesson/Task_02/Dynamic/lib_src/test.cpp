0d273320d2ad47863e1e136021a6ef5f81ad2175
Ostap Pukalskyy <ostap.pukalskyy@gmail.com>
2021-12-22 08:57:23 +0200

    added task 2

---
+++ 2_lesson/Task_02/Dynamic/lib_src/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "test.hpp"
+
+
+void test_01()
+{
+	std::cout << "Test 01 executed\n";
+}
+
+
+void test_02()
+{
+	std::cout << "Test 02 executed\n";
+}
+
---
+++ 2_lesson/Task_02/Static/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../lib_src/test.hpp"
+
+
+
+
+int main(int argc, char **argv)
+{    
+	test_01();
+	test_02();
+	
+	return 0;
+
+}
