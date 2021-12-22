8faa12a0e5fb90cdb2c52205eec5ea81be8cabb1
Ostap Pukalskyy <ostap.pukalskyy@gmail.com>
2021-12-22 09:50:19 +0200

    added task 3

---
+++ 2_lesson/Task_03/src/main.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+
+int main(int argc, char **argv)
+{    
+
+	// load of user32.dll will be done at this point
+	MessageBox(NULL, "Hello", "Hello", MB_OK);
+	
+	
+
+	return 0;
+
+}
